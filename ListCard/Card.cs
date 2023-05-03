171151d8fe9a0a79f278b1e97ca4b2fea505b832
jlbel <julia.belyaeva2012@gmail.com>
2023-05-03 15:46:57 +0300

    Добавьте файлы проекта.

---
+++ ListCard/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCard
+{
+    internal class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }  
+        public Card(Values value, Suits suit) 
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+        public string Name
+        {
+            get { return ($" {Value} of {Suit}"); }
+        }
+    }
+}
---
+++ ListCard/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListCard
+{
+    internal class Program
+    {
+        private static readonly Random random = new Random();
+        static Card RandomCard()
+        {
+            Card card = new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+            return card;
+        }
+        static void PrintCards(List<Card> cards)
+        {
+            foreach (Card card in cards) Console.WriteLine($"{card.Value} of {card.Suit}");
+        }
+        static void Main(string[] args)
+        {
+           List<Card> cards= new List<Card>();
+            IComparer<Card> comparer = new CardComparerByValue();
+            Console.WriteLine("Enter number of cards: ");
+            if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out int count))
+            {
+                for (int i = 0; i <= count; i++)
+                {
+                    cards.Add(RandomCard());
+                }
+                PrintCards(cards);
+                cards.Sort(comparer);
+                Console.WriteLine("........Sorting the card........");
+                PrintCards(cards);
+            }
+        }
+    }
+}
